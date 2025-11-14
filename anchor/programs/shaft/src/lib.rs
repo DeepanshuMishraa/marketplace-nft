e805ead90dd4572d022a64b06800b8c96b0998ed
Deepanshu Mishra <d4deepanshu723@gmail.com>
2025-11-14 22:42:18 +0530

    Done with Programs i guess , tests remaining

--- anchor/programs/shaft/src/lib.rs
+++ anchor/programs/shaft/src/lib.rs
@@ -1,8 +1,12 @@
 #![allow(clippy::result_large_err)]
 
 use anchor_lang::prelude::*;
+pub mod events;
 pub mod instructions;
 pub mod state;
+
+use events::*;
+use instructions::*;
 
 declare_id!("Count3AcZucFDPSFBAeHkQ6AvttieKUkyJ8HiQGhQwe");
 
@@ -10,10 +14,31 @@ declare_id!("Count3AcZucFDPSFBAeHkQ6AvttieKUkyJ8HiQGhQwe");
 pub mod shaft {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn list_nft(ctx: Context<Make>, price: u64) -> Result<()> {
+        ctx.accounts.init_escrow(price, &ctx.bumps)?;
+
+        ctx.accounts.deposit_nft()?;
+
+        emit!(ListingCreated {
+            maker: ctx.accounts.maker.key(),
+            mint_nft: ctx.accounts.mint_nft.key(),
+            price,
+            escrow: ctx.accounts.escrow.key(),
+        });
+
+        Ok(())
+    }
+
+    pub fn buy(ctx: Context<Buy>) -> Result<()> {
+        ctx.accounts.buy_nft()?;
+
+        emit!(ListingFinished {
+            maker: ctx.accounts.maker.key(),
+            taker: ctx.accounts.taker.key(),
+            price: ctx.accounts.escrow.price,
+            mint_nft: ctx.accounts.mint_nft.key(),
+        });
+        
         Ok(())
     }
 }
-
-#[derive(Accounts)]
-pub struct Initialize {}
